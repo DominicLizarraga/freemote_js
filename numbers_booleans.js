09d17267f45212a1b4b7b1aac479a7b4b6a8a03f
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-08-16 22:00:13 -0700

    if statement even or odd

---
+++ numbers_booleans.js
@@ -0,0 +1,24 @@
+const myMoney = 3021;
+
+console.log(myMoney % 2);
+
+console.log(myMoney % 0);
+
+
+const isEven = myMoney % 2
+
+if (myMoney % isEven === 0) { // even
+    console.log('odd');
+} else { // odd
+    console.log('even');
+}
+
+const anotherMoney = 301
+
+const isOdd = (anotherMoney % 2) === 0;
+
+if (isOdd) {
+    console.log('odd')
+} else {
+    console.log('even')
+}
