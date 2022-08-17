d60b8f9a91b879b3de8138d9f4edbf8f575ce1d2
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-08-16 18:29:03 -0700

    variables types

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("hello" + " " + "world");
+// let and const 
+
+// let num = 1; 
+// let num2 = 2;
+
+// console.log(num + num2)
+
+// console.log(num - num2)
+
+// console.log(num / num2)
+
+// console.log(num * num2)
+
+// reassigning let
+
+let num = 1; // first assignment
+let num2 = 2;
+console.log(num);
+num = 2; // second assignment
+console.log(num + num2);
+console.log(num);
+
+// difference btw let and const is semantic convention as HTML 
+// in JS you should be using const unless you know you'll re asign it
+
+const number = 1;
+const number2 = 3;
+console.log(number + number2);
+
+// var is for older version of JS
+var myVar = 13;
+// naming convention describe what the variable has be specific as decriptive as possible
+
+// nothing wrong with long variable names, don't use single letters
+
+const numOfPeople = 12; // camelCase; capitalize the beginning of each word
+
