597acaef78dc31d19678f5f61422269ce0dd845e
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-08-17 07:45:29 -0700

    template string and escaped

---
+++ template_strings_escaped_characters.js
@@ -0,0 +1,18 @@
+// template strings
+
+var firstName = "Dominic";
+
+var helloString = `<h1> Hello ${firstName} </h1>`
+
+console.log(helloString);
+
+// multi line paragraph
+
+var multiLine = `
+    <div>
+        <h1> first paragraph </h1>
+        <p> second paragraph </p>
+        <p> thrid paragraph</p>
+    </div>
+`; // could use \n in the end of each paragraph
+
