ab9076901dd39b6eec840aea27b003fbcf1653ad
DominicLizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-08-21 16:14:52 -0700

    js challenge 01

---
+++ js_challenge_01.js
@@ -0,0 +1,8 @@
+var printHelloName = function(firstName) {
+  // Your Code Here
+  // concat new var fullString with String Hello
+  var fullString = `Hello, ${firstName}!`
+  return fullString // return fullString
+};
+
+console.log(printHelloName("Aaron"));
